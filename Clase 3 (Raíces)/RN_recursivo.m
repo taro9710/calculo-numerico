8ccdb78c897eb696b2fbaf75eefb19a56a5f28a1
Lautaro Heredia <lauta9710@gmail.com>
2026-03-20 01:58:01 -0300

    Problema 1.2.4 proyectil completado y comentado

--- Clase 3 (Raíces)/RN_recursivo.m
+++ Clase 3 (Raíces)/RN_recursivo.m
@@ -1,8 +1,9 @@
-function raiz = RN_recursivo(x,tol)
+function [raiz,iter] = RN_recursivo(x,tol,n=1)
   f = @(y) x.^2 - 4;
   df = @(y) 2*x;
+  iter= n;
   raiz = x - (f(x)/df(x));
   if abs(f(raiz)) > tol
-    raiz = RN_recursivo(raiz, tol);
+    [raiz,iter] = RN_recursivo(raiz, tol,n+1);
   endif
 endfunction
--- Clase 3 (Raíces)/proyectil.m
+++ Clase 3 (Raíces)/proyectil.m
@@ -1,19 +1,34 @@
-function [t,error,n] = proyectil(t,tol)
+function proyectil(t,tol)
+#{
+  La velocidad vertical de despegue v de un cohete próximo a la superficie
+  terrestre se puede aproximarmediante v(t) = u*log(M0/(M0-mdot*t)) - g*t,
+  Quiero conocer el tiempo t en el cuál se alcanza la velocidad del sonido. Para
+  comstruyo una función f(t)= v(t) - v_sonido, donde f(t) tendrá una raíz cuando
+  v(t) = v_sonido y utilizo el método de Newton-Raphson para resolverlo
+  para las pruebas utilizaré un t inicial de 10s y una tolerancia de 0,000001
+  #}
+  # Seteo los parámetros iniciales.
   u = 2510;
   M0 = 2.8e6;
   mdot = 13.3e3;
   g = 9.81;
   v = 335;
+  # Defino f(t)
   f = @(t) u*log(M0/(M0-mdot*t)) - g*t - v;
+  # Defino df/dt
   df = @(t) (u*mdot)/(M0-mdot*t) - g;
-  error = 999;
+  error = abs(f(t));
   n = 0;
   while error > tol
-      t_new = t - f(t)/df(t);
+      t_new = t - f(t)/df(t); #Utilizo la funcón iteración de
       error = abs(f(t_new));
       t = t_new;
       n = n + 1;
   end
-
+  % Resultado
+  fprintf("Metodo: Newton-Raphson\n");
+  fprintf("Tiempo t = %.6f s\n", t);
+  fprintf("Error |f(t)| = %.6e\n", error);
+  fprintf("Iteraciones = %d\n", n);
 
   endfunction
